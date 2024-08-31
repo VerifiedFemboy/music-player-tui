1e9a54b3e4b17d36b0af63320bfd97bfaadc62e8
verifiedfemboy <maciekplaygamesyt@gmail.com>
2024-08-31 17:30:00 +0200

    jaja bruh

--- src/app.rs
+++ src/app.rs
@@ -9,37 +9,51 @@ pub struct App {
     player: Player,
     directory: PathBuf,  // Przechowuje ścieżkę do katalogu
     selected_file: Option<PathBuf>,
+    index_select: usize,
     input: InputMode,
     search_input: String,
 }
 
 impl App {
-    // Inicjalizacja aplikacji
+
     pub fn new(player: Player, directory: PathBuf) -> Self {
         Self {
             player,
             directory,
             selected_file: None,
+            index_select: 0,
             input: InputMode::Normal,
             search_input: String::new(),
         }
     }
 
-    // Uruchomienie aplikacji
+
     pub fn run<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> io::Result<()> {
         loop {
             terminal.draw(|frame| ui(frame, &self))?;
             
             if let Event::Key(key) = event::read()? {
                 match self.input {
-                    InputMode::Search => {},
-                    InputMode::Select => {},
+                    InputMode::Search => match key.code {
+                        KeyCode::Esc | KeyCode::Tab => self.change_input(InputMode::Normal),
+                        _ => {},
+                    },
+                    InputMode::Select => match key.code {
+                        KeyCode::Esc => self.change_input(InputMode::Normal),
+                        KeyCode::Up => self.select_up(),
+                        KeyCode::Down => self.select_down(),
+                        KeyCode::Enter => self.play_the_selected_file(),
+                        _ => {},
+                    },
                     InputMode::Playback => match key.code {
-                        _ => {}
+                        KeyCode::Esc => self.change_input(InputMode::Normal),
+                        _ => {},
                     },
                     InputMode::Normal => match key.code {
-                        KeyCode::Char('q') => break,
-                        KeyCode::Char(':') => {},
+                        KeyCode::Char('q') | KeyCode::Esc => break,
+                        KeyCode::Char('s') => self.change_input(InputMode::Select),
+                        KeyCode::Char(' ') => self.change_input(InputMode::Playback),
+                        KeyCode::Tab => self.change_input(InputMode::Search),
                         _ => {},
                     },
                 }
@@ -47,16 +61,53 @@ impl App {
         }
         Ok(())
     }
+
+    fn change_input(&mut self, input: InputMode) {
+        self.input = input;
+    }
+
+    fn select_up(&mut self) {
+        if self.index_select > 0 {
+            self.index_select -= 1;
+        }
+    }
+
+    fn select_down(&mut self) {
+        let entries_max = match fs::read_dir(&self.directory) {
+            Ok(entries) => entries.count().saturating_sub(1),
+            Err(_) => 0,
+        };
+
+        if self.index_select < entries_max {
+            self.index_select += 1;
+        }
+    }
+
+    fn play_the_selected_file(&mut self) {
+        if let Ok(mut entries) = fs::read_dir(&self.directory) {
+            if let Some(entry) = entries.nth(self.index_select) {
+                if let Ok(entry) = entry {
+                    self.selected_file = Some(entry.path());
+                    
+                    if let Some(ref path) = self.selected_file {
+                        self.player.change(path);
+                        self.player.play();
+                    }
+                }
+            }
+        }
+    }
+    
+
 }
 
-// Renderowanie interfejsu użytkownika
 fn ui(frame: &mut Frame, app: &App) {
     let vertical = Layout::vertical([
-        Constraint::Length(3), Constraint::Length(10), Constraint::Min(1)
+        Constraint::Length(3), Constraint::Length(10), Constraint::Min(1), Constraint::Max(1)
     ]);
 
     // Search Area
-    let [search_area, select_area, playback_area] = vertical.areas(frame.area());
+    let [search_area, select_area, playback_area, help_info_area] = vertical.areas(frame.area());
 
     let search = Paragraph::new(app.search_input.as_str())
         .style(match app.input {
@@ -74,20 +125,33 @@ fn ui(frame: &mut Frame, app: &App) {
             .map(|(i, entry)| {
                 let entry = entry.unwrap();
                 let filename = entry.file_name().to_string_lossy().into_owned();
-                let content = Line::from(Span::raw(format!("{i}: {}", filename)));
-                ListItem::new(content)
+                let content = Line::from(Span::raw(format!("{}", filename)));
+            
+                let mut item = ListItem::new(content);
+
+                if i == app.index_select {
+                    let style = Style::default().fg(Color::LightCyan);
+                    item = item.style(style);
+                }
+
+                item
             })
             .collect(),
         Err(_) => vec![ListItem::new("Brak plików lub błąd dostępu")],
     };
 
-    let files = List::new(files).block(Block::bordered().title("Files"));
+    let files = List::new(files).block(Block::bordered()
+    .style(match app.input {
+        InputMode::Select => Style::default().fg(Color::LightMagenta),
+        _ => Style::default(),
+    })
+    .title("Files"));
     frame.render_widget(files, select_area);
 
 
     //Playback Area
     let playback_info = match &app.selected_file {
-        Some(file) => format!("Odtwarzanie: {}", file.file_name().unwrap().to_string_lossy()),
+        Some(file) => format!("Playing: {}", file.file_name().unwrap().to_string_lossy()),
         None => "Brak pliku do odtwarzania".to_string(),
     };
     
@@ -96,9 +160,9 @@ fn ui(frame: &mut Frame, app: &App) {
         .block(Block::bordered().title("Playback"));
 
     frame.render_widget(playback, playback_area);
+
 }
 
-// Tryby wprowadzania
 enum InputMode {
     Search,
     Select,
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,7 @@
-use std::{fs::File, io::{self, BufReader, Write}};
+use std::{fs::File, io::{self, BufReader}, path::PathBuf};
 use rodio::{Decoder, OutputStreamHandle, Sink};
 
 pub(crate) struct Player {
-    current_playing: String,
     paused: bool,
     sink: Sink,
 }
@@ -10,32 +9,39 @@ pub(crate) struct Player {
 impl Player {
     pub fn new(stream_handle: OutputStreamHandle ) -> Self {
         Self { 
-            current_playing: "".to_string(), 
             paused: true, 
-            sink: Sink::try_new(&stream_handle).unwrap() 
+            sink: Sink::try_new(&stream_handle).unwrap(),
+            
         }
     }
 
-    pub fn add(&mut self, file: File) {
-        let source = Decoder::new(BufReader::new(file)).unwrap();
-        self.sink.append(source);
-        self.current_playing = "Your Track Name".to_string(); // Możesz dodać logikę do ustalania nazwy utworu
-        self.paused = true;
+    fn open_file(path: &PathBuf) -> io::Result<File> {
+        let file = File::open(path)?;
+        Ok(file)
     }
+
+    pub fn change(&mut self, path: &PathBuf) {
+        match Self::open_file(path) {
+            Ok(file) => {
+                let source = Decoder::new(BufReader::new(file)).unwrap();
+                self.sink.append(source);
+                self.paused = false;
+            }
+            Err(_) => panic!("Something went wrong while opening file..."),
+        }
+    }
+    
 
     pub fn play(&mut self) {
         if self.paused {
             self.sink.play();
             self.paused = false;
-            println!("Resuming playback.");
-        } else {
-            println!("Already playing.");
         }
     }
 
     pub fn pause(&mut self) {
         if !self.paused {
-            self.sink.pause(); // Użyj `pause`, aby wstrzymać bez zatrzymywania odtwarzania
+            self.sink.pause();
             self.paused = true;
             println!("Paused.");
         } else {
@@ -43,22 +49,4 @@ impl Player {
         }
     }
 
-    pub fn handle(&mut self) {
-        loop {
-            let mut command = String::new();
-            print!("Enter command (play/pause/quit): ");
-            io::stdout().flush().unwrap();
-            io::stdin().read_line(&mut command).unwrap();
-
-            match command.trim() {
-                "play" => self.play(),
-                "pause" => self.pause(),
-                "quit" => {
-                    self.sink.stop(); // Opcjonalnie zatrzymaj odtwarzanie przy wyjściu
-                    break;
-                }
-                _ => println!("Unknown command!"),
-            }
-        }
-    }
 }
